c77e1578b5533acdc0b46669becf32924b1afe4d
Ana <anaflaviamandello@gmail.com>
2023-12-07 17:28:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using BlogASPNET.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+app.MapControllers();
+
+app.Run();
